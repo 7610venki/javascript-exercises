798f356e61d10cf85abf01077af66bb3b3624556
7610venki <97399325+7610venki@users.noreply.github.com>
2024-09-12 15:30:10 +0400

    Exercise 1 & 2 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+const repeatString = function(nameToBeRepeated,NoOfTimes) {
+let result="";
+if(NoOfTimes>=0){
+for(let i=0;i<NoOfTimes;i++){
+result+=nameToBeRepeated;
+}   
+return result;
+}
+else{return "ERROR"}
+}
 
 // Do not edit below this line
 module.exports = repeatString;
