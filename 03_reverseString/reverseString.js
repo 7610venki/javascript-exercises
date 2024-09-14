7a67e0b54691e77ebbab7082094c4a2602a8b456
7610venki <97399325+7610venki@users.noreply.github.com>
2024-09-14 17:16:11 +0400

    Excercise 3 & 4 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+let initial = text;
+let reverese="";
+for(let i=initial.length-1; i>=0;i--){
+reverese+=initial[i];
+}
+return reverese;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...args) {
+
+    let receivedArray  = inputArray;
+
+    for (let index = 0; index < args.length; index++) {
+        let num = receivedArray.indexOf(args[index]);
+
+    while (receivedArray.includes(args[index])) {
+        receivedArray.splice(num,1);
+        }   
+    }
+    return receivedArray;
 };
 
 // Do not edit below this line
