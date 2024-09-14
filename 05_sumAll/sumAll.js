d30273b1dda19283d1328c0e76a7ff8c7c2eebfb
7610venki <97399325+7610venki@users.noreply.github.com>
2024-09-14 22:33:38 +0400

    Excercise 5 & 6 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
 
+    let result=0;
+    if (Number.isInteger(num1) && Number.isInteger(num2) && num1>-1 && num2>-1)  {
+            if(num1<num2){
+            while (num1 <=num2) {
+                result+=num1;
+                num1++;
+            }
+            return result;
+           } 
+           else{
+            while (num2 <=num1) {
+                result+=num2;
+                num2++;
+            }
+            return result;
+           }
+    }
+    else {
+        return "ERROR";  
+        }
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,26 @@
-const leapYears = function() {
-
+const leapYears = function(lp) {
+if(lp%400==0 && lp%100==0){
+        return true;
+    }
+else if (lp%4==0){
+    if(lp%100==0){
+        return false;
+    }
+    else {return true;}
+}
+else {return false}
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+//optimized logic
+// Leap Year Rule:
+// A year is a leap year if:
+// It is divisible by 400 or
+// It is divisible by 4 and not divisible by 100.
+
+// const leapYears = function(lp) {
+//     return (lp % 400 === 0) || (lp % 100 !== 0 && lp % 4 === 0);
+//   };
+  
