ae93bcd1d2897426993970d8976683082407f65a
7610venki <97399325+7610venki@users.noreply.github.com>
2024-09-19 14:45:56 +0400

    Completed 11,12 Excercises

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
+let arr=[];
+
+    for (const book of books) {
+        console.log(book);
+        arr.push(book.title);
+    }
+console.log(arr);
+return arr;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,36 @@
-const findTheOldest = function() {
+const findTheOldest = function(persons) {
+    
+const currentYear = new Date().getFullYear();
+
+let arr = persons.map((person)=> {
+    if(person.yearOfDeath==undefined){
+        person.yearOfDeath =currentYear;
+    }
+    if(person.yearOfBirth==undefined){
+        person.yearOfBirth =currentYear;
+    }
+    let age = person.yearOfDeath-person.yearOfBirth;
+    return { name: person.name, age: age };
+})
+console.log("Refined array with age calculation "+arr);
+//
+let newArray=[];
+for (const ar of arr) {
+    newArray.push(ar.age);
+}
+console.log("Age only array data is " + newArray);
+//
+newArray.sort((a,b)=>a-b);
+let getAge = newArray[newArray.length-1];
+console.log("Highest Age is "+getAge)
+//
+for (const newAr of arr) {
+    if(newAr.age==getAge){
+        console.log(typeof newAr.name)
+        return newAr;
+        
+    }
+}
 
 };
 
